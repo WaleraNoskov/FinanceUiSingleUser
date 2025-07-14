4c8bd53db45044d499a6cfdf13ec14a9e2065ccf
Valery Noskov <rahdah06@gmail.com>
2025-07-14 15:51:23 +0300

    feat: added boards management ui

---
+++ FinanceUi.Windows/FinanceUi.Windows/Feature/BoardsManagement/Pages/BoardsManagementPage/BoardsManagementViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FinanceUi.Windows.Feature.BoardsManagement.Controls.BoardsSearchBar;
+using FinanceUi.Windows.Feature.Shared;
+
+namespace FinanceUi.Windows.Feature.BoardsManagement.Pages.BoardsManagementPage;
+
+internal class BoardsManagementViewModel : ObservableObject
+{
+	private readonly BoardsSearchBarViewModel _boardSearchBarViewModel;
+	public BoardsSearchBarViewModel BoardSearchBarViewModel => _boardSearchBarViewModel;
+
+	public BoardsManagementViewModel(BoardsSearchBarViewModel boardsSearchBarViewModel)
+	{
+		_boardSearchBarViewModel = boardsSearchBarViewModel;
+	}
+}
